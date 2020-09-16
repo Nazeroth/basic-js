51ef8915c42ddc5a6312234ee0b63fc8a9a05ddc
Nazeroth <danieldefoe22@gmail.com>
2020-09-16 04:00:32 +0300

    refactor: rewrite code with ternary operator

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,10 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  if (!Array.isArray(members)) return false;
-  return members
-    .filter((a) => typeof a == "string")
-    .map((arr) => arr.trim().substring(0, 1).toUpperCase())
-    .sort()
-    .join("");
+  return !Array.isArray(members) ? false : members.filter((a) => typeof a == "string").map((arr) => arr.trim().substring(0, 1).toUpperCase()).sort().join("");
 };
