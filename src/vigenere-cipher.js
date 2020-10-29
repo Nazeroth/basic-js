2f6f618cb7d9c54827b7064f1eb49c338d1a749e
Nazeroth <danieldefoe22@gmail.com>
2020-10-29 22:55:05 +0200

    add: extended repeater & vigenere cuipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,58 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(doublesided = "true") {
+    this.doublesided = doublesided;
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw Error;
+    }
+
+    let messageUpper = message.toUpperCase();
+    let keyFull = key.padEnd(message.length, key).toUpperCase();
+    let res = "";
+    let index = 0;
+
+    for (let i = 0; i < messageUpper.length; i += 1) {
+      let charCode = messageUpper.charCodeAt(i);
+      if (charCode >= 65 && charCode <= 91) {
+        charCode -= 65;
+        let encrypted = keyFull.charCodeAt(index) - 65;
+        let resCode = ((charCode + encrypted) % 26) + 65;
+        res += String.fromCharCode(resCode);
+        index++;
+      } else {
+        res += messageUpper[i];
+      }
+    }
+    return !this.doublesided ? res.split("").reverse().join("") : res;
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw Error;
+    }
+
+    let messageUpper = message.toUpperCase();
+    let keyFull = key.padEnd(message.length, key).toUpperCase();
+    let res = "";
+    let index = 0;
+
+    for (let i = 0; i < messageUpper.length; i++) {
+      let charCode = messageUpper.charCodeAt(i);
+      if (charCode >= 65 && charCode <= 91) {
+        charCode -= 65;
+        let encrypted = keyFull.charCodeAt(index) - 65;
+        let resCode = ((charCode + 26 - encrypted) % 26) + 65;
+        res += String.fromCharCode(resCode);
+        index++;
+      } else {
+        res += messageUpper[i];
+      }
+    }
+    return !this.doublesided ? res.split("").reverse().join("") : res;
   }
 }
 
