642d165644fbdb4e53a9b8e0d548a67f06336ce0
Nazeroth <danieldefoe22@gmail.com>
2020-10-29 22:55:05 +0200

    add: extended repeater & vigenere cipher

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let repeatTimes = 0
+  let separator = '+';
+  let addition = '';
+  let additionRepeatTimes = 0;
+  let additionSeparator = '|';
+  if (str === null) {
+    str = 'null'
+  }
+  if (str === true) {
+    str = 'true'
+  }
+  if (options['repeatTimes'] !== undefined ) {
+    repeatTimes = options['repeatTimes'] - 1
+  }
+  if (options['separator'] !== undefined) {
+    separator = options['separator'];
+  }
+  if (options['addition'] !== undefined) {
+    addition = options['addition'];
+  }
+  if (options['additionRepeatTimes'] !== 0) {
+    additionRepeatTimes = options['additionRepeatTimes'] - 1;
+  }
+  if (options['additionSeparator'] !== undefined) {
+    additionSeparator = options['additionSeparator'];
+  }
+  let zeroPattern = str + addition;
+  let firstPattern = `${additionSeparator}${addition}`.repeat(additionRepeatTimes);
+  let secondPattern = `${separator}${zeroPattern}${firstPattern}`.repeat(repeatTimes);
+  
+  return zeroPattern + firstPattern + secondPattern;
+
 };
   
